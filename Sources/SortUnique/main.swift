b50b30c78ebe3a5412fbf7b2942edb0356af72be
Stijn Willems <stijn.willems@ing.com>
2021-01-26 16:43:40 +0100

    allow comma separated stuff

--- Sources/SortUnique/main.swift
+++ Sources/SortUnique/main.swift
@@ -8,5 +8,15 @@ guard let clip = clipboardContent() else {
     exit(EXIT_FAILURE)
 }
 
-let uniqueContent = Set(clip.components(separatedBy: "\n"))
-print(uniqueContent.sorted().joined(separator: "\n"))
+let lines = clip.components(separatedBy: "\n")
+let commaSeparated = clip.components(separatedBy: ",").map { $0.trimmingCharacters(in: .whitespacesAndNewlines)}
+if lines.count > 1 {
+    let uniqueContent = Set(lines)
+    print(uniqueContent.sorted().joined(separator: "\n"))
+} else if commaSeparated.count > 1 {
+    let uniqueContent = Set(commaSeparated)
+    print(uniqueContent.sorted().joined(separator: ", "))
+} else {
+    print("❌ not comma, nor line separated")
+}
+
